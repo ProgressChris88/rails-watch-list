cb635477c27480827263f6a55cdf755b7c1f65ac
Christopher Bowman <chrisbowmanfl@gmail.com>
2023-07-10 14:14:56 +0900

    finished bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,7 +7,7 @@ class BookmarksController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list = @List
+    @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
     else
